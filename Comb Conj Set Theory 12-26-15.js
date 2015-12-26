7d68e25a84c803ee7c72bb18f1b1c336a3cb357d
Solarus8 <jonvoelm@gmail.com>
2015-12-26 12:23:53 -0800

    update with better expansions of the primary numbers in Base 10

---
+++ Comb Conj Set Theory 12-26-15.js
@@ -0,0 +1,220 @@
+/**
+ * Created by Jonathan Christopher Voelm on 12/2/15.
+ * Conjunctive Combinatorial Set Theory
+ *
+ * Copy Right Jonathan Voelm 12-2-15.
+ */
+//Updated 12-26-15
+
+/**** Hand Calculated Pattern of the Conjunctive Combinatorial Set Theory ****
+ *
+ *
+ *
+ ****  START OF THEORY  ****************************************************************************************************************************************************************
+ *
+ *
+ *
+
+-------------------------------------------------------------------------------------------
+* ZERO
+
+ Meaning =  Earth, Origin, Empty Set, Sub-strait, Infinite Map  ==> time  ==> Hierarchical Symmetry
+ The Sphere
+
+
+ 0 =               [ { } ]  -->  1 first order & fundamental element --> [{infinite_symbol}, {InfSym}, {InfSym}, ...]  --> which is the set of all infinite sets, Hierarchically ordered
+ #expansion#          |     ==>  1 root element
+
+ 1 root point node  -->  Total number of elements, recursive counting of all {} sets contained in fundamental number 0
+
+ F_Conj_additive_single_decimal_compression_map(0) = 1
+
+
+-------------------------------------------------------------------------------------------------------------------------
+* ONE
+
+Meaning =  Air, Magnitude, Scalar Vector, 1D line, force, initiation, energy
+The tetrahedron
+
+1 =             [  {+(0, 1)  *2}   ,     {*(1 , 1}) ]   -->  2 first order elements
+#expansion#          /  /|   *2   &;       /|   /|
+#expansion#      ({}+{}+{})  *2   &;    ({}+{}+{}+{})
+#expansion#          3       *2    +         4
+               ({}+{}+{}) + ({}+{}+{})    +     ({}+{}+{}+{}) ==>  10 root elements
+
+
+10 root nodes  -->  Total number of end point elements, recursive counting of all {}  sets contained in fundamental number 1
+
+F_Conj_additive_single_decimal_compression_map(1) = 1
+
+
+-------------------------------------------------------------------------------------------------------------------------
+*TWO
+
+ Meaning = Water, Space Manifest in 2D Shape, Triangle, Anaerobic Life, Light Cone
+ The Cube
+
+
+ 2 =             [ {+(0, 2) *2}   ,   {+(1,1)}   ,  {*(1, 2) *2}     ]   -->  3 first order elements
+ #expansion#         /  /|| *2  &;      /| /|   &;   /| /|| *2
+ #expansion#             8       +    4     +      10        ==> 22 root elements
+
+
+ 22 root nodes  -->  Total number of elements, recursive counting of all {} sets contained in fundamental number 2
+
+ F_Conj_additive_single_decimal_compression_map(2) = 4
+
+
+---------------------------------------------------------------------------------------------------------------------------
+* THREE
+
+Meaning = Air, Manifest in 3D Solid, Tetrahedron, Energy, Spirit, Aerobic Life ==> Spatial Field
+The Octahedron
+
+
+3 =            [ +(0,  3 ) *2}  ,   {+(1, 2) *2}   ,   {*(3 ,   1)* 2}   ,  {+(1, 1, 1}) ]   -->  4 first order elements
+#expansion#       /  //||  *2  &;    /| /|| *2   &;    //||  /| * 2   &;  /| /| /|
+#expansion#          10         +       10        +        12         +      6       ==>  38 root elements
+
+
+38 root nodes   -->  Total number of elements, recursive counting of all {} sets contained in fundamental number 3
+
+F_Conj_additive_single_decimal_compression_map(3) = 2
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+*  FOUR
+
+ Meaning = Consciousness, Magic of Form,  (Foundation)  ==> Spirit and Matter United
+ The Dodecahedron
+
+
+ 4 =            [ {+(0,  4   ) *2} ,  {  *(1,    4)    *2}  ,   { +(2,   2)    *2} ,     {+(  1  , 3)  *2}   ,   {+( 1 , 1 ,  2) *3}   ,  {+(1 ,1 ,1 ,1}  ]   -->  6 first order elements
+ #expansion#         /  ///||| *2 &;      /|  ///|||  *2    &;     /||   /||  *2  &;         /|   //||  *2  &;     /|  /|  //|  *3    &;  /| /| /| /|  &;
+ #expansion#            14         +        16              +       12             +          12             +           21           +          8           ==>  83 root elements
+
+
+ 83 root nodes   -->  Total number of elements, recursive counting of all {} sets contained in fundamental number 4
+
+ F_Conj_additive_single_decimal_compression_map(4) = 4
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+*  FIVE
+
+Meaning = Synthesis, Power Step, Completion into Hierarchical Recursion Manifest, Meta-verse - Symmetry point for local Universal time-space-energy
+The Icosahedron   -->  The last perfect geometric solid
+
+
+ 5 =                [ {+(0,   5)   *2} ,      {+(1,   4)   *2}   ,  {+(2,    3) *2 }   , {*(1,    5)   *2}   ,  {+(1,  2,   2)  *3}  ,  {+(1,  3,    1) *3}  ,  {+(1,  1,  1,  2) *4}  , {+(1,  1,  1,  1,  1)} ]  ---> 8 first order elements
+ #expansion#           /  ////\\\\ *2 &:        /| ///|||  *2   &;   //|   //|| *2    &;   /| ////|||| *2   &;    /|  //| //|   *#  &;   /|  //||   /|  *3) &;    /|  /|  /| //|  *4  &;   /|  /|  /|  /|  /|
+ #expansion count#         16         +            16            +       14            +        20           +       24              +        24             +          27             +         10       ===========>  143   root elements
+
+
+ 174   -->  Total number of elements, recursive counting of all {} sets contained in fundamental number 5
+
+ F_Conj_additive_single_decimal_compression_map(5) = 3
+
+
+ -------------------------------------------------------------------------------------------
+ *  SIX
+ *  Meaning =  Double spirit, star of david, 1 number past the brockenly semetric number, perfection in spirit, 3 on 3.
+
+ 6 =              [ {+(0,   6)     *2}      ,  {+(1,     5)   *2}  ,  {+(2,   4  )  *2}    ,  {+(3,   3)}   ,  {*(1,      6)    *2}    , {*(2,    3) *2}  ,  {+(1,  1,  4 ) *3}    , {+(1,  2,   3  )   *6}  ,  {+( 1, 1,   2,   2) *6 } , {+(1,  1,   1,  1,   2)  *5} , {+(1,  1,  1,  1,  1,  1)}  ] ---> 11 first order elements
+ #expansion#         /  //////|||||    *2 &;   /|  ////||||  *2  &;   //\  ///|||      &;  //||  //|| &;     /|  //////||||| *2  &;  //\  //|| *2  &;   /|  /|  ///|||   &;  /|  //|  //||    *6  &;    /|  /|  //|  //|  *6  &;  /|   /|  /|   /|  //|  *5 ;&       /|  /|  /|  /|  /|  /|
+ #expansion count#      12         *2      +        10       *2   +      9        *2   +       8      +           13         *2  +      7      *2   +          10          +       9        *6     +           10         *6   +            11           *5  +             12
+                             24            +            20       +        18           +       8      +             26           +         14       +          30          +            54         +               60          +                   55        +             12    ===>  301 root elements
+
+ 276   -->  Total number of elements, recursive counting of all {} sets contained in fundamental number 6
+
+ F_Conj_additive_single_decimal_compression_map(6) = 6      // self map!!!
+
+ *
+ *
+ *
+ *****   START OF COMPUTATION SCRIPT    ******************************************************************************************************************************************
+ *
+ *
+ *
+ */
+
+// Function to output calculations
+function Cset_internal_Nums_Calc(cal_real_decimal_set_character) {
+
+// initialize the array for zero which is the null set and counts as a 1 element set
+    var Conjunctive_Array =[0];
+
+//  Define the passed in variable as the fundamental decimal number to compute it's set characteristics
+    var in_dec_num = cal_real_decimal_set_character;
+
+    if (in_dec_num == 0) {
+        //  Print out the input
+        console.log("For Numberal: ");
+        console.log(in_dec_num);
+        console.log("\r");
+
+        //  Print out the array
+        console.log("The Array Looks like: ");
+        Conjunctive_Arrar = [];
+        console.log(Conjunctive_Array);
+        console.log("\r");
+
+        //  Print 1st order element count in the Set for input decimal number
+        primary_elements = 1;
+        console.log("The 1st order elements count =  ");
+        console.log(primary_elements);
+        console.log("\r");
+    }
+
+    if (in_dec_num == 1) {
+        Conjunctive_Array = [[[1,0],2], [1,1]];
+    }
+
+//  Print out the input
+    console.log("For Numberal: ");
+    console.log(in_dec_num);
+    console.log("\r");
+
+//  Print out the array
+    console.log("The Array Looks like: ");
+    console.log(Conjunctive_Array);
+    console.log("\r");
+
+//  Print 1st order element count in the Set for input decimal number
+    primary_elements = Conjunctive_Array.length;
+    console.log("The 1st order elements count =  ");
+    console.log(primary_elements);
+    console.log("\r");
+
+// Print out root nodes number ----   for 1 this should be 10
+    var count = 0;  //Initialize count of root nodes at 0
+    for (var i = 0; i<=Conjunctive_Array.length; i++) {
+        if (Conjunctive_Array[i].length == 1) {
+            count = count++;
+        }
+        else if (Conjunctive_Array[i].length >=1) {
+            var index_offset =Conjunctive_Array[i++];
+            count = count++;
+        }
+    };
+        console.log("The root node element count = ");
+        console.log(count);
+        console.log("");
+}
+
+/*    Make a recursion function to calaculate out need first order elements.
+
+      Have that function pust the results into a 2D array that will hold the map value for the set.
+
+      This Array will be use to speed up calculaton by using a stack and iterate method.
+
+      So -- Let's try splitting up the work...........   :)   :)    :)
+ *
+ *
+ *
+ * */
+
+
+
+
+//Test the function with a call
+Cset_internal_Nums_Calc(2);
